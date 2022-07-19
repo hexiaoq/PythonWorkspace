e9342dfc4996186179626fc29ffb6ca5135fdab1
hxq <156437734@qq.com>
2022-07-19 21:56:41 +0800

    增加Person类

--- test3/SetAndDict.py
+++ test3/SetAndDict.py
@@ -11,3 +11,15 @@ print(ord(num))
 mul = [i for i in range(100) if i % 5 == 0]
 print(mul)
 
+class Person:
+    name = 'hxq'
+    age = 23
+    salary = 10000
+
+    def my_salry(self):
+        print('你的月收入为:%d' %self.salary)
+
+hxq = Person()
+print(hxq.age)
+hxq.my_salry()
+
